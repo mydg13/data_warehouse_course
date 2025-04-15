82970383205b4882cb50b67299139ed8360c899a
mydg13 <mydang1306@gmail.com>
2025-04-15 15:40:36 +0700

    extract layer

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,27 @@
+WITH dim_product__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+)
+
+, dim_product__rename AS (
+  SELECT 
+    stock_item_id AS  product_key
+    , stock_item_name AS product_name
+    , brand AS	brand_name
+  FROM dim_product__source
+)
+
+, dim_product__cast_type AS (
+  SELECT 
+    CAST(product_key AS INTEGER) AS product_key
+    , CAST(product_name AS STRING) AS product_name
+    , CAST(brand_name AS STRING) AS brand_name
+  FROM dim_product__rename
+)
+
 SELECT 
-  CAST( stock_item_id AS integer) AS  product_key
-  , CAST( stock_item_name AS string) AS product_name
-  , CAST( brand AS string) AS	brand_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+  product_key
+  , product_name
+  , brand_name
+FROM dim_product__cast_type
+
