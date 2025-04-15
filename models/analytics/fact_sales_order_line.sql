992f93119e8c8ed92e984d6cd284feb63e174024
mydg13 <mydang1306@gmail.com>
2025-04-15 15:29:31 +0700

    change type fact

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,7 @@
 SELECT 
-  order_line_id AS sales_order_line_key
-  , stock_item_id AS product_key
-  , quantity
-  , unit_price
-  , quantity * unit_price AS gross_amount
+  CAST (order_line_id AS integer) AS sales_order_line_key
+  , CAST (stock_item_id AS integer) AS product_key
+  , CAST (quantity AS integer) AS quantity
+  , CAST (unit_price AS numeric ) AS unit_price 
+  , CAST (quantity AS integer) *  CAST (unit_price AS numeric ) AS gross_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
