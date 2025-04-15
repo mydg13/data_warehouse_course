a853d2c1e1f8f6db44d8e5c90c51e68d95f0fa14
mydg13 <mydang1306@gmail.com>
2025-04-15 16:01:58 +0700

    creat customer dim

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,23 @@
+WITH dim_customer__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.sales__customers`
+)
+
+, dim_customer__rename AS (
+  SELECT 
+    customer_id AS  customer_key
+    , customer_name AS customer_name
+  FROM dim_customer__source
+)
+
+, dim_customer__cast_type AS (
+  SELECT 
+    CAST(customer_key AS INTEGER) AS customer_key
+    , CAST(customer_name AS STRING) AS customer_name
+  FROM dim_customer__rename
+)
+
 SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+  customer_key
+  , customer_name
+FROM dim_customer__cast_type
