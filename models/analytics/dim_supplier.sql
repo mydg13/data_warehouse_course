9a103f38341184c3a70f222f91e90c18939cb17e
mydg13 <mydang1306@gmail.com>
2025-04-17 19:13:33 +0700

    creat_dim_supplier

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,23 @@
+WITH supplier__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+)
+
+, supplier__rename AS (
+  SELECT 
+    supplier_id AS  supplier_key
+    , supplier_name
+  FROM supplier__source
+)
+
+, supplier__cast_type AS (
+  SELECT 
+    CAST(supplier_key AS INTEGER) AS supplier_key
+    , CAST(supplier_name AS STRING) AS supplier_name
+  FROM supplier__rename
+)
+
 SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+  supplier_key
+  , supplier_name
+FROM supplier__cast_type
